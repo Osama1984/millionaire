87fffa555fa86c9a06d1ca27eeed87a90ca9d7ae
Osama1984 <oneamah@gmail.com>
2024-07-14 23:57:31 -0400

    chnge 7

--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,22 +1,27 @@
-import { AfterContentChecked, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Question } from '../../interfaces/game';
 import { CommonModule } from '@angular/common';
 import { GameService } from '../../services/game.service';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-answers',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './answers.component.html',
-  styleUrl: './answers.component.scss'
+  styleUrls: ['./answers.component.scss']
 })
 export class AnswersComponent implements OnInit {
-  @Input() question!: Question|null;
-  answers!:string[];
-  level!:number
-  constructor(private service:GameService){}
+  @Input() question!: Question | null;
+  answers: string[] = [];
+  level = 0;
+
+  constructor(private service: GameService) {}
+
   ngOnInit(): void {
-    this.service.getCurrentQuestion().subscribe((data: Question | null) => {
+    this.service.getCurrentQuestion().pipe(
+      distinctUntilChanged((prev, curr) => prev?.question === curr?.question)
+    ).subscribe((data: Question | null) => {
       if (data) {
         const incorrectAnswers = data.incorrect_answers ?? [];
         const correctAnswer = data.correct_answer ?? '';
@@ -24,68 +29,70 @@ export class AnswersComponent implements OnInit {
       }
     });
   }
+
   setAnswers(answers: string[]): void {
-      this.answers = this.shuffleAnswers(answers);
+    this.answers = this.shuffleAnswers(answers);
   }
+
   shuffleAnswers(answers: string[]): string[] {
-      return answers.sort(() => Math.random() - 0.5);
+    return answers.sort(() => Math.random() - 0.5);
   }
-  selectAnswer(answer:string){
-    if(answer===this.question?.correct_answer){
+
+  selectAnswer(answer: string): void {
+    if (answer === this.question?.correct_answer) {
       let score = 0;
-      switch(this.level){
+      switch (this.level) {
         case 0:
-          score=100;
+          score = 100;
           break;
         case 1:
-          score=200;
+          score = 200;
           break;
         case 2:
-          score=300;
+          score = 300;
           break;
         case 3:
-          score=500;
+          score = 500;
           break;
         case 4:
-          score=1000;
+          score = 1000;
           break;
         case 5:
-          score=2000;
+          score = 2000;
           break;
         case 6:
-            score=4000;
-            break;
+          score = 4000;
+          break;
         case 7:
-            score=8000;
-            break;
+          score = 8000;
+          break;
         case 8:
-            score=16000;
-            break;
+          score = 16000;
+          break;
         case 9:
-            score=32000;
-            break;
+          score = 32000;
+          break;
         case 10:
-            score=64000;
-            break;
+          score = 64000;
+          break;
         case 11:
-            score=125000;
-            break;
+          score = 125000;
+          break;
         case 12:
-            score=250000;
-            break;
+          score = 250000;
+          break;
         case 13:
-            score=500000;
-            break;
+          score = 500000;
+          break;
         case 14:
-              score=1000000;
-              break;
+          score = 1000000;
+          break;
         default:
-          score=0;
+          score = 0;
       }
-      this.service.updateScore(score)
-      this.service.setCurrentLevel(this.level+1)
-    }
-    else{
+      this.service.updateScore(score);
+      this.service.setCurrentLevel(this.level + 1);
+    } else {
       this.service.setGameEnd();
     }
   }
