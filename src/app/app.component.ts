1f857e1062854d7ca0dba7702cb268f0368e8b24
Osama1984 <oneamah@gmail.com>
2024-07-15 23:38:59 -0400

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { MillionaireModuleModule } from './millionaire-module/millionaire-module
 import { CommonModule } from '@angular/common';
 import { GameService } from './services/game.service';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+import { NgxSpinnerModule } from "ngx-spinner";
+import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MillionaireModuleModule, CommonModule],
+  imports: [RouterOutlet, MillionaireModuleModule, CommonModule, NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -18,11 +19,19 @@ export class AppComponent implements OnInit {
   module: MillionaireModuleModule = new MillionaireModuleModule();
   loading$: Observable<boolean>;
 
-  constructor(private service: GameService) {
+  constructor(private service: GameService, private spinner: NgxSpinnerService) {
     this.loading$ = this.service.getLoading();
   }
 
   ngOnInit(): void {
+        /** spinner starts on init */
+        this.spinner.show();
+
+        setTimeout(() => {
+          /** spinner ends after 5 seconds */
+          this.spinner.hide();
+        }, 5000);
+    
     this.getStartValue().subscribe((data: boolean) => {
       if (data === true) {
         this.service.loadQuestions();
